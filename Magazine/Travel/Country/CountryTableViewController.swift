25d6409177aecf54f238cd93b3cc9ff44056bd7b
우다당탕 개발자 <uddt_ds@kakao.com>
2025-07-15 20:55:33 +0900

    [Feat] 여행 국가 탐색 화면 구현 * 세그먼트 선택 시 초기에는 변경이 되지만, 이후에 변경이 안되는 오류가 있어 수정 예정

--- Magazine/Travel/Country/CountryTableViewController.swift
+++ Magazine/Travel/Country/CountryTableViewController.swift
@@ -9,8 +9,70 @@ import UIKit
 
 class CountryTableViewController: UITableViewController {
 
+    @IBOutlet var segMenu: UISegmentedControl!
+    
+    let cityData = CityInfo()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigation()
+        segmentedMenuUI()
+
+        let nib = UINib(nibName: String(describing: CountryTableViewCell.self), bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: String(describing: CountryTableViewCell.self))
     }
 
+    private func setupNavigation() {
+        navigationController?.navigationBar.scrollEdgeAppearance = .init()
+        navigationItem.title = "인기 도시"
+    }
+
+    private func segmentedMenuUI() {
+        segMenu.selectedSegmentIndex = 0
+        segMenu.setTitle("모두", forSegmentAt: 0)
+        segMenu.setTitle("국내", forSegmentAt: 1)
+        segMenu.insertSegment(withTitle: "해외", at: 2, animated: true)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch segMenu.selectedSegmentIndex {
+        case 0:
+            return cityData.city.count
+        case 1:
+            return cityData.domesticData.count
+        case 2:
+            return cityData.overseaData.count
+        default:
+            return 0
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch segMenu.selectedSegmentIndex {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: CountryTableViewCell.self), for: indexPath) as! CountryTableViewCell
+
+            cell.configureCell(with: cityData.city[indexPath.row])
+            return cell
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: CountryTableViewCell.self), for: indexPath) as! CountryTableViewCell
+
+            cell.configureCell(with: cityData.domesticData[indexPath.row])
+            return cell
+        case 2:
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: CountryTableViewCell.self), for: indexPath) as! CountryTableViewCell
+            cell.configureCell(with: cityData.overseaData[indexPath.row])
+        default:
+            return UITableViewCell()
+        }
+        return UITableViewCell()
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+
+    @IBAction func segmentedTapped(_ sender: UISegmentedControl) {
+        tableView.reloadData()
+    }
 }
---
+++ Magazine/Travel/Model/City.swift
@@ -0,0 +1,24 @@
+//
+//  City.swift
+//  Magazine
+//
+//  Created by Lee on 7/15/25.
+//
+
+import Foundation
+
+struct City {
+    let cityName: String
+    let cityEnglishName: String
+    let cityExplain: String
+    let cityImage: String
+    let domesticTravel: Bool
+
+    init(city_name: String, city_english_name: String, city_explain: String, city_image: String, domestic_travel: Bool) {
+        self.cityName = city_name
+        self.cityEnglishName = city_english_name
+        self.cityExplain = city_explain
+        self.cityImage = city_image
+        self.domesticTravel = domestic_travel
+    }
+}
